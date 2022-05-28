de629fd615db28b30cfb891077abf187eecf7ee4
A. Nataprawira Darmawan <abimanyudarmawan4@gmail.com>
2022-05-28 17:24:40 +0700

    updates

--- components/Main.jsx
+++ components/Main.jsx
@@ -7,16 +7,16 @@ const Main = () => {
     <div className='w-full h-screen text-center'>
         <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
             <div>
-                <p className='uppercase text-sm tracking-widest text-gray-600'>
+                <p className='font-Rubik uppercase text-sm tracking-widest text-gray-600'>
                     LET'S BUILD SOMETHING TOGETHER
                 </p>
-                <h1 className='py-4 text-gray-700'>
+                <h1 className='font-Rubik py-4 text-gray-700'>
                     Hi, I'm <span className='text-[#5651e5]'>Darmawan</span>
                 </h1>
-                <h1 className='py-2 text-gray-700'>
+                <h1 className='font-Rubik py-2 text-gray-700'>
                     A Front-End Web Developer
                 </h1>
-                <p className='py-4 text-gray-600 max-w-[70%] m-auto'>
+                <p className='font-Rubik py-4 text-gray-600 max-w-[70%] m-auto'>
                     I'm a front-end web developer specializing in building exceptional
                     digital experiences. Currently focused on building responsive front-end
                     web applications while learning back-end technologies.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Skills from '../components/Skills'
+import About from '../components/About'
 import Main from '../components/Main'
 import Navbar from '../components/Navbar'
 
@@ -13,6 +15,8 @@ export default function Home() {
       </Head>
       <Navbar/>
       <Main/>
+      <About/>
+      <Skills/>
     </div>
   )
 }
