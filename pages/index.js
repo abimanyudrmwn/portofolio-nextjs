ef75bb2aad4c72a2b5eae91c673e6bd1e4a7be9d
A. Nataprawira Darmawan <abimanyudarmawan4@gmail.com>
2022-05-28 17:43:51 +0700

    finalupdate

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Contact from '../components/Contact'
 import Skills from '../components/Skills'
 import About from '../components/About'
+import Projects from '../components/Projects'
 import Main from '../components/Main'
 import Navbar from '../components/Navbar'
 
@@ -17,6 +19,8 @@ export default function Home() {
       <Main/>
       <About/>
       <Skills/>
+      <Projects/>
+      <Contact/>
     </div>
   )
 }
